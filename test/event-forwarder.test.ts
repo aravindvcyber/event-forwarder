185a78dd872c354b7a78109aa578f53b995eeae4
Aravind Vadamalaimuthu <aravindvcyber@gmail.com>
2022-08-19 11:00:38 +0530

    Initial commit

---
+++ test/event-forwarder.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EventForwarder from '../lib/event-forwarder-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/event-forwarder-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EventForwarder.EventForwarderStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
