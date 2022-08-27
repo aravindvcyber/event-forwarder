abebced98fb43db714dbd4a2204e4a80641ab802
Aravind Vadamalaimuthu <aravindvcyber@gmail.com>
2022-08-27 21:43:52 +0530

    commit

--- lambda/stack-event-processor.ts
+++ lambda/stack-event-processor.ts
@@ -105,7 +105,7 @@ const handle = async function (event: SQSEvent) {
 
         keyPairList.push({ PK: item.stackId.S, SK: item.time.N });
 
-        collection.push(generateInnerSection(item));
+        collection.push(generateInnerSection(item, region));
       });
     }
 
